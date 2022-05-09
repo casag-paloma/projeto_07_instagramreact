cfbd34d6934bb1da59474df07f515c5795f9583b
Paloma <casagrande.paloma@gmail.com>
2022-05-09 17:04:11 -0300

    fear bônus: curtidas

--- src/PastaCorpo/PastaEsquerda/FundoAcoes.js
+++ src/PastaCorpo/PastaEsquerda/FundoAcoes.js
@@ -1,8 +1,24 @@
+import React from 'react';
+
 export default function FundoAcoes(){
-    const icones = ["heart-outline", "chatbubble-outline", "paper-plane-outline"];
+    const [icone, setIcone] = React.useState("heart-outline");
+    const [cor, setCor] = React.useState("normal");
+    
+    function Likar(){
+        setIcone("heart");
+        setCor("vermelho");
+    }
+
+    function Deslikar(){
+        setIcone("heart-outline");
+        setCor("normal");
+    }
+    
+    const icones = ["chatbubble-outline", "paper-plane-outline"];
     return(
         <div class="acoes">
             <div>
+                <ion-icon name={icone} class={cor} onClick={(icone === "heart-outline") ? Likar : Deslikar}></ion-icon>
                 {icones.map(icone => <ion-icon name={icone}></ion-icon> )}
             </div>
             <div>
@@ -11,3 +27,4 @@ export default function FundoAcoes(){
         </div>
     );
 };
+
